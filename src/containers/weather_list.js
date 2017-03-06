fddc0649175c2a0045aa96c2c82a1613a96c8863
Jordy Webb <jordywebb@Jordys-MacBook.local>
2017-03-06 08:05:25 -0800

    add table rows

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,18 +2,30 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+      </tr>
+    );
+
+  }
+
   render() {
     return (
       <table className="table table-hover">
         <thead>
           <tr>
             <th>City</th>
-            <th>Temp</th>
+            <th>Temperature</th>
             <th>Pressure</th>
             <th>Humidity</th>
           </tr>
         </thead>
         <tbody>
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
 
